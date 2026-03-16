178b0851c2d40131d7083146029ef167189d236c
Otaviofertig <204613470+Otaviofertig@users.noreply.github.com>
2026-03-16 19:53:37 -0300

    commit

--- cadastro.c
+++ cadastro.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 
 int main () { 
+    // Variáveis
     char nome[50];
-    char cpf[11];
+    char sexo;
+    char cpf[13];
+    float peso, altura;
+    int dia, mes, ano;
 
-    printf("Digite seu primeiro nome: ")
+    
+    printf("Digite seu primeiro nome: ");
     scanf("%s", nome);
 
-    printf("Digite seu cpf: ")
+    printf("Digite seu cpf: ");
     scanf("%s", cpf);
 
-    printf("--SEUS DADOS--");
-    printf("Nome: %s\n", nome);
+    printf("Sexo (m/f): ");
+    scanf(" %c", &sexo); 
+
+    printf("Data de nascimento\n");
+    printf("Dia: ");
+    scanf("%d", &dia);
+
+    printf("Mes: ");
+    scanf("%d", &mes);
+
+    printf("Ano: ");
+    scanf("%d", &ano);
+
+    printf("Peso: ");
+    scanf("%f", &peso); 
+
+    printf("Altura: ");
+    scanf("%f", &altura); 
+
+   
+    printf("\n--SEUS DADOS--\n"); 
     printf("CPF: %s\n", cpf);
+    printf("Sexo: %c\n", sexo);
+    printf("Nascimento: %02d/%02d/%d\n", dia, mes, ano); 
+    printf("Peso: %.2f\n", peso);
+    printf("Altura: %.2f\n", altura);
+
+    return 0;
 }
